e613a3f14dd9904ff3502513a628aa059ccf65ce
Averlarque <elijah.zaharov@gmail.com>
2016-10-07 15:06:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ from handlers import *
 from report_templates import *
 import os
 
+# Init Bootle.py app
 app = Bottle()
 
-# GLOBALS
+# Init Jinja2 environmemt
 j2_env = Environment(loader=FileSystemLoader(temp_path))
 
 
+# Render html-templates
 def render(tpl, context):
 	return j2_env.get_template(tpl).render(context)
 
 
+# Report page controller
 @app.route('/report')
 def report():
 	data = get_datetime()
@@ -23,7 +26,7 @@ def report():
 	           'till_time': till[0], 'till_date': till[1]}
 	return render('report.tpl', context)
 
-
+# Report page receiver
 @app.route('/report', method='POST')
 def make_report():
 	since = {
@@ -42,7 +45,7 @@ def make_report():
 	report_format = request.forms.get('format')
 
 	report_class = request.forms.get('report_class')
-
+    # Launch report generation
 	if report_class == 'project':
 		project = request.forms.get('project')
 		reporter = ProjectPeriodReport(
@@ -61,10 +64,10 @@ def make_report():
 			time_period[0], time_period[1], report_format=report_format,
 			report_type=report_type)
 		reporter.generate_report()
-	
 	return render('status.tpl', {'text': report_ok})
 
 
+# Login page controller
 @app.route('/login')
 def login():
 	try:
@@ -78,6 +81,7 @@ def login():
 	return render('login.tpl', context)
 
 
+# Login page receiver
 @app.route('/login', method='POST')
 def do_login():
 	dct = {'host': request.forms.get('host'),
@@ -85,6 +89,7 @@ def do_login():
 	       'time': str(today)[:19]}
 	password = request.forms.get('password')
 	check = login_check(dct, password)
+	# Check access to the host
 	if check[0] is True:
 		dct.update({'api': check[1]})
 		p_handler(dct)
@@ -94,6 +99,7 @@ def do_login():
 		return render('status.tpl', {'text': login_fail})
 
 
+# Settings page controller
 @app.route('/settings')
 def settings():
 	data = p_load()
@@ -105,27 +111,31 @@ def settings():
 	return render('settings.tpl', context)
 
 
+# Reports folder opener
 @app.route('/reports_folder')
 def reports_folder():
 	open_reports_folder()
+	# Redirect back to the settings
 	redirect('/settings')
 
 
+# About page controller
 @app.route('/about')
 def about():
 	context = {'name': 1}
 	return render('about.tpl', context)
 
 
+# Static files handler
 @app.get('/static/<filename:path>')
 def send_static(filename):
 	return static_file(filename, root=static_path)
 
-
+# 404 error handler
 def error404(error):
 	return render('status.tpl', {'text': e404})
 
-
+# 500 error handler
 def error500(error):
 	return render('status.tpl', {'text': e500})
 
@@ -140,6 +150,7 @@ if __name__ == '__main__':
 	port = 8060
 	from threading import Thread
 	app_thread = Thread(target=run, kwargs=dict(app=app, host='localhost', port=port))
+	# Starting the app
 	app_thread.start()
 	# Open the web browser
 	webbrowser.open('http://localhost:{0}/login'.format(port))
